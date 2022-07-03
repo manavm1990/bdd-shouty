7e18c989c69417e9bb1ab0eaf0cf8da597df4945
Manav Misra <manavm@visionwebsoft.com>
2022-07-03 10:01:17 -0500

    test: add sample 🥒 implementation for feature

---
+++ features/step_definitions/steps.mjs
@@ -0,0 +1,23 @@
+import { Given, Then, When } from "@cucumber/cucumber";
+
+Given(
+  "Lucy is located {int} meters from Sean",
+  (int) =>
+    // Given('Lucy is located {float} meters from Sean', function (float) {
+    // Write code here that turns the phrase above into concrete actions
+    "pending"
+);
+
+When(
+  "Sean shouts, {string}",
+  (string) =>
+    // Write code here that turns the phrase above into concrete actions
+    "pending"
+);
+
+Then(
+  "Lucy hears Sean's message",
+  () =>
+    // Write code here that turns the phrase above into concrete actions
+    "pending"
+);
