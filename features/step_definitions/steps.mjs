1797d2307304e1daf11e17fb5cd5fe7f3ec58f44
Manav Misra <manavm@visionwebsoft.com>
2022-07-03 10:05:09 -0500

    Log generated value of parameter from Gherkin

--- features/step_definitions/steps.mjs
+++ features/step_definitions/steps.mjs
@@ -1,12 +1,13 @@
 import { Given, Then, When } from "@cucumber/cucumber";
 
-Given(
-  "Lucy is located {int} meters from Sean",
-  (int) =>
-    // Given('Lucy is located {float} meters from Sean', function (float) {
-    // Write code here that turns the phrase above into concrete actions
-    "pending"
-);
+Given("Lucy is located {int} meters from Sean", (distance) => {
+  // {int} is automatically passed in from Gherkin verbiage where it mentions '15'.
+  console.log(distance);
+
+  // Given('Lucy is located {float} meters from Sean', function (float) {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
 
 When(
   "Sean shouts, {string}",
