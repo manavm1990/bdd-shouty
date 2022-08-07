4c25b4b7aef5fa04fc13c5ccd13217074d04cc5b
Manav Misra <manavm@visionwebsoft.com>
2022-08-07 16:08:29 -0500

    Simplify custom parameter type

--- features/support/person_parameter.mjs
+++ features/support/person_parameter.mjs
@@ -1,20 +1,11 @@
 import { defineParameterType } from "@cucumber/cucumber";
-import Person from "../../app/models/Person.js";
 
 defineParameterType({
   name: "listener",
   regexp: /[a-zA-Z]+/,
-  type: Person,
-  transformer(name) {
-    return new Person(name);
-  },
 });
 
 defineParameterType({
   name: "shouter",
   regexp: /[a-zA-Z]+/,
-  type: Person,
-  transformer(name) {
-    return new Person(name);
-  },
 });
