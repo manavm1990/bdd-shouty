e57b333e206a42d0856de9d6de03b3b3fe6a5ed9
Manav Misra <manavm@visionwebsoft.com>
2022-08-20 12:40:01 -0500

    fix: #listeners is private

--- app/models/Network.js
+++ app/models/Network.js
@@ -1,13 +1,17 @@
 export default class Network {
-  constructor() {
-    this.listeners = [];
+  #listeners;
+
+  constructor(range = 15) {
+    this.range = range;
+
+    this.#listeners = [];
   }
 
   subscribe(listener) {
-    this.listeners.push(listener);
+    this.#listeners.push(listener);
   }
 
   broadcast(message) {
-    this.listeners.forEach((listener) => listener.hear(message));
+    this.#listeners.forEach((listener) => listener.hear(message));
   }
 }
